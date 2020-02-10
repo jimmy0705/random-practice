4eefddf160639e69b848f5b1609e16c9f2441bbe
juned <junedsarwar@gmail.com>
2020-02-10 22:12:18 +0000

    practice1

--- index.js
+++ index.js
@@ -23,10 +23,25 @@ Person.prototype.lastName = "cena";
 var john = new Person('john',1990,'teacher');
 var jimmy = new Person('jimmy',1980,'banker');
 var ttt = new Person();
-console.log(john);
-console.log(jimmy);
-console.log(john.lastName);
-console.log(jimmy.lastName);
+// console.log(john);
+// console.log(jimmy);
+// console.log(john.lastName);
+// console.log(jimmy.lastName);
 //jimmy.calculate();
 
 
+var Wallet = function (num1,num2){
+  this.money = num1;
+  this.spend = num2
+}
+
+Wallet.prototype.remain= function(){
+  console.log(this.money-this.spend);
+}
+
+
+var x1 = new Wallet(2000,178);
+
+
+console.log(x1);
+x1.remain();

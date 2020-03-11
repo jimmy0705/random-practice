7a7220d8ed32e26f48f30e96c5a52f411e3b58aa
juned123 <juned@frrole.com>
2020-03-11 23:55:38 +0530

    dom practice

---
+++ DOM/dom.js
@@ -0,0 +1,19 @@
+console.log("js file connected");
+
+var paras = document.querySelectorAll("p");
+//var para = document.getElementsByTagName("p");
+//console.log(paras);
+
+paras.forEach(para=>{
+   // console.log(para);
+})
+
+var err = document.querySelector("div.error");
+//console.log(err);
+
+var errs = document.querySelectorAll(".error");
+
+errs.forEach(para=>{
+    // console.log(para);
+ })
+ 
--- practice.js
+++ practice.js
@@ -10,7 +10,40 @@ for(let i =0;i<str1.length;i++){
 let num1= 20;
 let i =0;
 while(i<num1){
-    console.log(i);
+    //console.log(i);
     i++;
 
 }
+//=============//
+
+let arr1= [10,20,30,100,80];
+
+for(let i =0;i<arr1.length-1;i++){
+    //console.log(arr1[i]);
+    if(arr1[i]===100){
+       // console.log("got 100");
+        break;
+    }
+}
+//=======function witgh default paramaters=========//
+
+var testing =function (name = "jimmy",age="26"){
+   // console.log(name,age);
+
+}
+
+testing();
+testing("john","20");
+
+//======================//
+
+function practice(name){
+    let j = name;
+   // console.log(j);
+   return j;
+}
+//console.log(j);
+const answ=practice("jimmyyy");
+//console.log(answ);
+
+
